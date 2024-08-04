91558f7dee9721cc77f34f3bc54b49674b2b31af
Sammy Mishinev <sam.mishinev@gmail.com>
2024-08-04 19:34:11 +0300

    Add animation flappy birds

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
 WIDTH = 2289  # Scale to 50x50 pixels
 HEIGHT = 1148
 
+current_frame = 0
+frame_count = 0
+frame_interval = 15
+
 background = pygame.image.load('images/background.png')
 scaled_background = pygame.transform.scale(background, (2289, 1482))
 pygame.image.save(scaled_background, 'images/background.png')
@@ -22,51 +26,45 @@ scaled_bird_down = pygame.transform.scale(bird_down, (125, 125))
 pygame.image.save(scaled_bird_down, 'images/flappy_bird_down.png')
 
 bird_up = Actor("flappy_bird_up")
-bird_up.pos = 150, 500
+bird_up.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
 
 bird_down = Actor("flappy_bird_down")
-bird_down.pos = 150, 500
-
-bird = Actor("flappy_bird_up")
-
-pipes = []
+bird_down.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
 
 
-def add_pipes():
-    gap_start = random.randint(100, 100)
-
-    pipe_up = Actor("pipe_up")
-    pipe_down = Actor("pipe_down")
-
-    top_pipe.pos = WIDTH, gap_start - top_pipe.height // 2
-    bottom_pipe.pos = WIDTH, gap_start + GAP + bottom_pipe.height // 2
-    pipes.append(pipe_up, pipe_down)
-
-
-def draw_pipes():
-    for pipe_pair in pipes:
-        pipe_pair[0].draw()
-        pipe_pair[1].draw()
-
-
-def update_pipes():
-    global pipes
-    for pipe_pair in pipes:
-        pipe_pair[0].x -= 5
-        pipe_pair[1].x -= 5
-    pipes = [pipe_pair for pipe_pair in pipes if pipe_pair[0].right > 0]
+def add_pipe():
+    pass
 
 
 def update():
+    global frame_count, current_frame
+    frame_count += 1
     if keyboard.space:
-        bird.y -= 5
-    bird.y += 3
-    update_pipes()
+        bird_up.y -= 10
+        bird_down.y -= 10
+
+        if bird_up.angle <= 50 and bird_down.angle <= 50:
+            bird_up.angle += 5
+            bird_down.angle += 5
+    else:
+        bird_up.angle = -30
+        bird_down.angle = -30
+
+    bird_up.y += 5
+    bird_down.y += 5
+
+    if frame_count >= frame_interval:
+        frame_count = 0
+        current_frame = 1 - current_frame
 
 
 def draw():
     screen.blit("background", (0, 0))
-    bird.draw()
+
+    if current_frame == 0:
+        bird_up.draw()
+    else:
+        bird_down.draw()
 
 
 pgzrun.go()
