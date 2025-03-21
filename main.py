da4e732e3f6ad219b7278b2aaa19c5fe09e3c081
Sammy Mishinev <sam.mishinev@gmail.com>
2025-03-21 14:24:36 +0200

    testing

--- main.py
+++ main.py
@@ -3,27 +3,54 @@ import pgzero
 import os
 import random
 import pgzrun
+from mako.compat import jython
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
 
+game_stop = False
+
 PIPE_GAP = 750
-PIPE_SPEED = 10
+PIPE_SPEED_ORIGIN = 5
 PIPE_INTERVAL = 400
 PIPE_COUNT = 2
 
+PIPE_SCORE = 0
+
+BIRD_SPEED_UP = 15
+BIRD_SPEED_DOWN = 7
+
 current_frame = 0
 frame_count = 0
-frame_interval = 15
+frame_interval = 10
 
-game_stop = False
+pipes = []
+pipe_timer = 0
+
+def set_fullscreen():
+    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+def get_WIDTH_and_HEIGHT():
+    info = pygame.display.Info()
+    global WIDTH, HEIGHT
+    WIDTH, HEIGHT = info.current_w, info.current_h
+
+set_fullscreen()
+get_WIDTH_and_HEIGHT()
 
 bird_up = pygame.image.load('images/flappy_bird_up.png')
 scaled_bird_up = pygame.transform.scale(bird_up, (125, 125))
 pygame.image.save(scaled_bird_up, 'images/flappy_bird_up.png')
 
+bird_up = Actor("flappy_bird_up")
+bird_up.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
+
+
 bird_down = pygame.image.load('images/flappy_bird_down.png')
 scaled_bird_down = pygame.transform.scale(bird_down, (125, 125))
 pygame.image.save(scaled_bird_down, 'images/flappy_bird_down.png')
+
+bird_down = Actor("flappy_bird_down")
+bird_down.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
 
 pipe_down = pygame.image.load('images/pipe_down.png')
 scaled_pipe_down = pygame.transform.scale(pipe_down, (100, 750))
@@ -33,40 +60,13 @@ pipe_up = pygame.image.load('images/pipe_up.png')
 scaled_pipe_up = pygame.transform.scale(pipe_up, (100, 700))
 pygame.image.save(scaled_pipe_up, 'images/pipe_up.png')
 
-
-def set_fullscreen():
-    pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-
-
-def get_WIDTH_and_HEIGHT():
-    info = pygame.display.Info()
-    global WIDTH, HEIGHT
-    WIDTH, HEIGHT = info.current_w, info.current_h
-    print(f"{WIDTH} {HEIGHT}")
-
-
-set_fullscreen()
-get_WIDTH_and_HEIGHT()
-
-bird_up = Actor("flappy_bird_up")
-bird_up.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
-
-bird_down = Actor("flappy_bird_down")
-bird_down.pos = WIDTH / 2 - 700, HEIGHT / 2 - 300
-
-pipes = []
-pipe_timer = 0  # Timer for controlling pipe spawning
-
-
 def add_pipes():
     pipe_height = random.randint(0, HEIGHT - PIPE_GAP)
     top_pipe = Actor("pipe_up", (WIDTH, pipe_height - 250))
     bottom_pipe = Actor("pipe_down", (WIDTH, pipe_height + PIPE_GAP + 110))
     pipes.append((top_pipe, bottom_pipe))
 
-
 add_pipes()
-
 
 def recycle_pipe(pipe_pair):
     if not game_stop:
@@ -75,9 +75,8 @@ def recycle_pipe(pipe_pair):
         top_pipe.pos = (WIDTH, pipe_height - 250)
         bottom_pipe.pos = (WIDTH, pipe_height + PIPE_GAP + 110)
 
-
 def check_collision():
-    global game_stop  # Declare game_stop as global to modify it
+    global game_stop
     if not game_stop:
         bird = bird_up if current_frame == 0 else bird_down
 
@@ -86,7 +85,6 @@ def check_collision():
 
                 game_stop = True
 
-
 def update():
     if not game_stop:
         global frame_count, current_frame, pipe_timer
@@ -94,8 +92,8 @@ def update():
         pipe_timer += 1
 
         if keyboard.space:
-            bird_up.y -= 10
-            bird_down.y -= 10
+            bird_up.y -= BIRD_SPEED_UP
+            bird_down.y -= BIRD_SPEED_UP
 
             if bird_up.angle <= 50 and bird_down.angle <= 50:
                 bird_up.angle += 5
@@ -104,18 +102,18 @@ def update():
             bird_up.angle = -30
             bird_down.angle = -30
 
-        bird_up.y += 5
-        bird_down.y += 5
+        bird_up.y += BIRD_SPEED_DOWN
+        bird_down.y += BIRD_SPEED_DOWN
 
         if frame_count >= frame_interval:
             frame_count = 0
             current_frame = 1 - current_frame
 
         for top_pipe, bottom_pipe in pipes:
-            top_pipe.x -= PIPE_SPEED
-            bottom_pipe.x -= PIPE_SPEED
+            top_pipe.x -= PIPE_SPEED_ORIGIN
+            bottom_pipe.x -= PIPE_SPEED_ORIGIN
 
-            if top_pipe.right < 0:  # If the pipe moves off the screen
+            if top_pipe.right < 0:
                 recycle_pipe((top_pipe, bottom_pipe))
 
         check_collision()
@@ -139,9 +137,5 @@ def draw():
         for top_pipe, bottom_pipe in pipes:
             top_pipe.draw()
             bottom_pipe.draw()
-
-
-
-
 
 pgzrun.go()
